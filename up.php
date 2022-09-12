cf2d381802dd491219465a39936e779a57255dcb
Spolokh <54540979+Spolokh@users.noreply.github.com>
2022-09-12 18:07:07 +0300

    Create up.php

---
+++ up.php
@@ -0,0 +1,40 @@
+<?php
+
+ini_set('display_errors', 0);
+
+/** MySQL settings */
+define('DBNAME', 'nordeuropa1');
+define('DBUSER', 'root');
+define('DBPASS', '');
+define('DBHOST', 'localhost');
+
+require_once 'libs/ORM.php';
+ORM::configure('mysql:host='.DBHOST.';dbname='.DBNAME);
+ORM::configure([
+    'username' => DBUSER,
+    'password' => DBPASS
+]);
+
+$db = ORM::getDb();
+
+$file = 'data.xml';
+file_exists($file) or die('Такого файла не существует');
+
+$file = simplexml_load_file($file);
+$data = $file->offers->offer;
+$values = [];
+$delete = [];
+
+foreach ($data as $row) 
+{
+    $row = (array) $row;
+    $delete[] = $row['id'];
+    $values[] = '("'.join('", "', array_values($row)).'")';
+}
+
+try {
+    $db->query('REPLACE INTO `products` VALUES '.join(', ', $values).';');
+    $db->query('DELETE FROM `products` WHERE `id` NOT IN ('.join(', ', $delete).');');
+} catch(\Exception $e) {
+    echo $e->getMessage();
+}
