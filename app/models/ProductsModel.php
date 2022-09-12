22b92595c76e23f04b6d871718b461ba977a35aa
Spolokh <54540979+Spolokh@users.noreply.github.com>
2022-09-12 18:17:45 +0300

    Update ProductsModel.php

--- app/models/ProductsModel.php
+++ app/models/ProductsModel.php
@@ -28,37 +28,37 @@ class ProductsModel extends Model
 		$total = $query->count();
 
 		$query = $query
-      ->whereRaw('(`mark` LIKE ? OR `model` LIKE ? OR `generation` LIKE ? OR `year` LIKE ? OR `color` LIKE ? OR `transmission` LIKE ?)', [
-          $search.'%', $search.'%',  $search.'%', $search.'%', $search.'%', $search.'%'
-      ])
-      ->offset($offset)
-      ->limit ($number)
-      ->orderByAsc('id')
-      ->findArray()
+		      ->whereRaw('(`mark` LIKE ? OR `model` LIKE ? OR `generation` LIKE ? OR `year` LIKE ? OR `color` LIKE ? OR `transmission` LIKE ?)', [
+			  $search.'%', $search.'%',  $search.'%', $search.'%', $search.'%', $search.'%'
+		      ])
+		      ->offset($offset)
+		      ->limit ($number)
+		      ->orderByAsc('id')
+		      ->findArray()
 		;
 		
 		foreach ($query AS $k => $row)
 		{
 			$data[] = [
-        'id'   => $row['id'],
-        'mark' => $row['mark'],
-        'model' => $row['model'],
-        'generation' => $row['generation'],
-        'year' => $row['year'],
-        'run' => $row['run'],
-        'color' => $row['color'],
-        'body'  => $row['body-type'],
-        'engine' => $row['engine-type'],
-        'transmission' => $row['transmission'],
-        'gear' => $row['gear-type']
+				'id'   => $row['id'],
+				'mark' => $row['mark'],
+				'model' => $row['model'],
+				'generation' => $row['generation'],
+				'year' => $row['year'],
+				'run' => $row['run'],
+				'color' => $row['color'],
+				'body'  => $row['body-type'],
+				'engine' => $row['engine-type'],
+				'transmission' => $row['transmission'],
+				'gear' => $row['gear-type']
 			];
 		}
 
 		return json_encode([
 			'draw' => $draw,
-        'recordsTotal' => $total,
-        'recordsFiltered' => $total,
-        'data' => $data
+			'recordsTotal' => $total,
+			'recordsFiltered' => $total,
+			'data' => $data
 		]);
 	}
 }
